e4fec41cc9bbed77c4314d999c12118ffbf66c19
jonhunt918 <74038462+jonhunt918@users.noreply.github.com>
2022-01-16 11:58:18 -0600

    Add files via upload

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,18 @@
+MongoDB: find()
+docs = db.collection_name.find({})
 
+for doc in docs:
+    print(doc)
+
+students.find({})
+
+MongoDB: find_one()
+doc = db.collection_name.find_one({"student_id": "1007"})
+
+MongoDB: find_one()
+doc = db.collection_name.find_one({"student_id": "1008"})
+
+MongoDB: find_one()
+doc = db.collection_name.find_one({"student_id": "1009"})
+
+print(doc["student_id"])
