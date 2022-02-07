0389dc3b3d6bfa322f8a7e4c591f7b329ac98eed
jonhunt918 <74038462+jonhunt918@users.noreply.github.com>
2022-02-07 13:59:36 -0600

    Update pysports_join_queries.py

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -1,1 +1,39 @@
+import mysql.connector
+from mysql.connector import errorcode
 
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT player_id, first_name, last_name, team_id FROM player INNER JOIN team ON player.team_id = team.team_id")
+
+    player = cursor.fetchall()
+
+    print("\n -- DISPLAYING PLAYER RECORDS -- ")
+
+    for player in player:
+        print("\n\n Player ID: {}\n First Name: {}\n Last Name: {}\n Team Name: {}".format(player[0], player[1], player[2], player[3]))
+
+    input("\n Press any key to continue... ")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_denied_ERROR:
+        print(" The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    db.close()
